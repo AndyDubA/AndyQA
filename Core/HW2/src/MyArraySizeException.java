13596c22a0a85e4efef2bc870fab3e68f3b81eff
AndyDubA <dubogips@mail.ru>
2021-09-06 21:51:22 +0500

    Initial commit.

---
+++ Core/HW2/src/MyArraySizeException.java
@@ -0,0 +1,13 @@
+public class HWmain {
+    public static void main(String args[]) {
+        try {
+            int[][] fxf = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+
+        } catch (My) {
+            System.out.println("Дeлeниe на ноль: " + e);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Ошибка индексации массива: " + e);
+        }
+        System.out.println("Пocлe блока операторов try/catch");
+    }
+}
